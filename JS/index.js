941164002437d79ba1730faf0ddee3992b245194
C3M3Z4 <cmeza@mezadigital.com>
2020-08-27 23:39:01 -0500

    Aplicando herencia en python y JS

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,10 @@
-var car = new Car("AW456", new Account("Andres Herrera", "QWE234"))
+  
+var car = new Car("AW456", new Account("Stephen Sauza", "QWE234"))
 car.passenger = 4;
 car.printDataCar();
 
+var uberX = new UberX("WIP234", new Account("Terance Felps", "QWE3462"), "Chevrolet", "Acadia")
+uberX.passenger = 7;
+uberX.printDataCar();
 
 
--- php/index.php
+++ php/index.php
@@ -1,10 +1,13 @@
 <?php 
-    require_once 'Car.php';
-    require_once 'Account.php';
+    require_once("Car.php");
+    require_once("UberX.php");
+    require_once("UberPool.php");
+    require_once("Account.php");
 
-    $car = new Car("CM982763", new Account("Cesar Octavio","MCCO97862"));
+    $uberX = new UberX("CM982763", new Account("Cesar Octavio", "MCCO97862"), "Chevrolet", "Spark");
+    $uberX->printDataCar();
 
-    print_r($car);
-    print_r($car->driver);
+    $uberPool = new UberPool("CM982763", new Account("JEsus Rdz", "MCCO2"), "Dodge", "Durango");
+    $uberPool->printDataCar();
 
 ?>
